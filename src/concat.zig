d0f0af1b899e02b65e943c523e9ddb3da2ab1b56
Evgenii Stratonikov <kraunid@gmail.com>
2019-10-27 19:11:50 +0300

    concat: accept std.ArrayList.Iterator instead of slice

--- src/concat.zig
+++ src/concat.zig
@@ -1,59 +1,71 @@
 const std = @import("std");
 const testing = std.testing;
 
+const ArrayList = std.ArrayList;
+const global_allocator = std.debug.global_allocator;
+
 fn concatT(comptime T: type) type {
     return struct {
         const Self = @This();
 
-        list1: []const T,
-        list2: []const T,
-
         const Iterator = struct {
-            list1: []const T,
-            list2: []const T,
-            _1: usize,
-            _2: usize,
+            iter1: ArrayList(T).Iterator,
+            iter2: ArrayList(T).Iterator,
+            end1: bool,
+            end2: bool,
 
-            fn next(self: *Iterator) ?T {
-                if (self._1 < self.list1.len) {
-                    const a = self.list1[self._1];
-                    self._1 = self._1 + 1;
-                    return a;
-                } else if (self._2 < self.list2.len) {
-                    const a = self.list2[self._2];
-                    self._2 = self._2 + 1;
-                    return a;
+            pub fn next(self: *Iterator) ?T {
+                var a: ?T = null;
+                if (!self.end1) {
+                    a = self.iter1.next();
+                    if (a == null) {
+                        self.end1 = true;
+                        a = self.iter2.next();
+                        if (a == null) {
+                            self.end2 = true;
+                        }
+                    }
+                } else if (!self.end2) {
+                    a = self.iter2.next();
+                    if (a == null) {
+                        self.end2 = true;
+                    }
                 }
-                return null;
+                return a;
             }
         };
 
-        fn init(list1: []const T, list2: []const T) Self {
-            return Self{
-                .list1 = list1,
-                .list2 = list2,
-            };
+        pub fn init() Self {
+            return Self{};
         }
 
-        fn iterator(self: *Self) Iterator {
+        pub fn iterator(self: *Self, iter1: ArrayList(T).Iterator, iter2: ArrayList(T).Iterator) Iterator {
             return Iterator{
-                .list1 = self.list1,
-                .list2 = self.list2,
-                ._1 = 0,
-                ._2 = 0,
+                .iter1 = iter1,
+                .iter2 = iter2,
+                .end1 = false,
+                .end2 = false,
             };
         }
     };
 }
 
-fn concat(list1: var, list2: var) concatT(@typeOf(list1).Child).Iterator {
-    const T = @typeOf(list1).Child;
-    return concatT(T).init(list1, list2).iterator();
+fn elemOf(comptime A: type) type { return std.meta.Child(A.Slice); }
+
+fn concat(list1: var, list2: var) concatT(elemOf(@typeOf(list1))).Iterator {
+    return concatT(elemOf(@typeOf(list1))).init().iterator(list1.iterator(), list2.iterator());
 }
 
 test "concat.non_empty" {
-    var list1 = ([]i32{1,2,3})[0..];
-    var list2 = ([]i32{4,5})[0..];
+    var list1 = std.ArrayList(i32).init(global_allocator);
+    defer list1.deinit();
+
+    var list2 = std.ArrayList(i32).init(global_allocator);
+    defer list2.deinit();
+
+    try list1.appendSlice([_]i32{1,2,3});
+    try list2.appendSlice([_]i32{4,5});
+    
     var iter = concat(list1, list2);
     testing.expect(iter.next().? == 1);
     testing.expect(iter.next().? == 2);
@@ -65,8 +77,15 @@ test "concat.non_empty" {
 }
 
 test "concat.first_empty" {
-    var list1 = ([]i32{})[0..];
-    var list2 = ([]i32{1,2})[0..];
+     var list1 = std.ArrayList(i32).init(global_allocator);
+    defer list1.deinit();
+
+    var list2 = std.ArrayList(i32).init(global_allocator);
+    defer list2.deinit();
+
+    try list1.appendSlice([_]i32{});
+    try list2.appendSlice([_]i32{1,2});
+
     var iter = concat(list1, list2);
     testing.expect(iter.next().? == 1);
     testing.expect(iter.next().? == 2);
@@ -75,8 +94,15 @@ test "concat.first_empty" {
 }
 
 test "concat.second_empty" {
-    var list1 = ([]i32{1})[0..];
-    var list2 = ([]i32{})[0..];
+    var list1 = std.ArrayList(i32).init(global_allocator);
+    defer list1.deinit();
+
+    var list2 = std.ArrayList(i32).init(global_allocator);
+    defer list2.deinit();
+
+    try list1.appendSlice([_]i32{1});
+    try list2.appendSlice([_]i32{});
+
     var iter = concat(list1, list2);
     testing.expect(iter.next().? == 1);
     testing.expect(iter.next() == null);
